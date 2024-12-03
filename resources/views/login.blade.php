d0c86db35e9fdb015351d42b4bafeea2adc395ed
flowsel <50051799+flowsel@users.noreply.github.com>
2024-12-03 14:04:31 +0700

    bugfix #12 (extend use)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
-
+@extends('layouts') <!-- Pastikan directive ini berada di paling atas -->
+@section('content') 
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
-
         <div class="card">
             <div class="card-header">Login</div>
             <div class="card-body">
@@ -15,18 +15,18 @@
                     <div class="mb-3 row">
                         <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email Address</label>
                         <div class="col-md-6">
-                            <input type="email" class="form-control @error('') is-invalid @enderror" name="" value="{{ old('') }}">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}">
+                            @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
                             @endif
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
                         <div class="col-md-6">
-                            <input type="password" class="form-control @error('') is-invalid @enderror" name="">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+                            @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
                             @endif
                         </div>
                     </div>
@@ -38,3 +38,4 @@
         </div>
     </div>
 </div>
+@endsection
