e4724a2b3220e27b5604dd2aa4fdf34db6d1b59d
flowsel <50051799+flowsel@users.noreply.github.com>
2024-12-03 13:22:01 +0700

    bugfix #11 (seeder)

--- database/seeders/TransaksiDetailSeeder.php
+++ database/seeders/TransaksiDetailSeeder.php
@@ -21,27 +21,28 @@ class TransaksiDetailSeeder extends Seeder
         $transaksi = Transaksi::all();
 
         foreach ($transaksi as $t) {
-            $numberOfDetails = // gunakan faker untuk membuat angka antara 5 - 15
+            $numberOfDetails = $faker->numberBetween(5, 15);
             $total_harga = 0;
 
             for ($j = 0; $j < $numberOfDetails; $j++) {
-                $hargaSatuan = $faker->numberBetween(10, 500) * 100;
+                $hargaSatuan = $faker->numberBetween(10, 500) * 100; // Harga satuan dalam kelipatan 100
                 $jumlah = $faker->numberBetween(1, 5);
                 $subtotal = $hargaSatuan * $jumlah;
-                $total_harga += $subtotal;
-
-                TransaksiDetail:create([
+                $total_harga += $subtotal; // Menambah subtotal ke total_harga
+                
+               TransaksiDetail::create([
                     'id_transaksi' => $t->id,
-                    'nama_produk' => $faker->productName,
+                    'nama_produk' => $faker->productName, 
                     'harga_satuan' => $hargaSatuan,
                     'jumlah' => $jumlah,
-                    'subtotal'
+                    'subtotal' => $subtotal,
                 ]);
             }
 
             $t->total_harga = $total_harga;
-            $t->bayar = ceil($total_harga/50000) * 50000;
-            $t->kembalian = $t->bayar - $total_harga
+            $t->bayar = ceil($total_harga / 50000) * 50000; 
+            $t->kembalian = $t->bayar - $total_harga;
+            $t->save(); 
         }
     }
 }
